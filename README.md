7912de73d5176d20fdaba98d8a1d940e480e8864
Gary Hsu <dreamfliper@gmail.com>
2025-06-13 17:58:15 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Features
+
+Record cursor positions and provide a command to quickly preview the cursor history.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 0.0.1
+
+Initial release
---
+++ src/extension.ts
@@ -0,0 +1,30 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode'
+
+// Array to keep track of cursor positions over time
+const cursorHistory: { documentUri: vscode.Uri; position: vscode.Position }[] = []
+
+// Listen to cursor (selection) changes in any active text editor
+const selectionListener = vscode.window.onDidChangeTextEditorSelection(event => {
+  // Only track the primary selection's active position
+  if (event.selections.length > 0) {
+    cursorHistory.push({ documentUri: event.textEditor.document.uri, position: event.selections[0].active })
+  }
+})
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(selectionListener)
+  const disposable = vscode.commands.registerCommand('cursor-location-preview.peek-history', () => {
+    const { document } = vscode.window.activeTextEditor!
+    vscode.commands.executeCommand(
+      'editor.action.peekLocations',
+      document.uri,
+      vscode.window.activeTextEditor?.selection.start,
+      cursorHistory.map(({ documentUri, position }) => new vscode.Location(documentUri, position))
+    )
+  })
+  context.subscriptions.push(disposable)
+}
+
+export function deactivate() {}
